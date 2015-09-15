600a7c1df07b59be5e2829eb3c2e85f52e686be9
Brian Brennan <brianb@cardsandpockets.com>
2015-09-15 13:53:02 -0400

    capAlpha works with some keys but not others

--- enigma.js
+++ enigma.js
@@ -1,8 +1,8 @@
 //settings
 
 var sequence = 'capAlpha'; // 'capAlpha', 'alpha', 'alphaNumeric', 'ascii'
-var rotorSettings = 'WELCOME';
-var plugSettings = ['SP','FE'];
+var rotorSettings = 'C';
+var plugSettings = ['NP','FE'];
 
 //functions
 
@@ -19,7 +19,7 @@ function enigmate(s){
 				r = rotorize(r);
 			}
 			atRotor = 0;
-			r = plugify(r);
+			// r = plugify(r);
 			message = message + r;
 		}
 
@@ -74,6 +74,7 @@ function plugify(c){
 			throw new Error('Can only PLUGIFY capital letters when SEQUENCE is set to "capAlpha"! \nEither change your SEQUENCE to the appropriate decyphering method, or change your input to be only cap letters');
 
 		for(var i = 0; i < plugSettings.length; i++){
+
 			var t = plugSettings[i].charAt(1);
 
 			if(!(t.charCodeAt(0) >= 65 && t.charCodeAt(0) <= 90))
@@ -88,6 +89,90 @@ function plugify(c){
 	return c;
 }//end of plugify
 
+function deEnigmate(s){
+	try{
+		if(typeof s !== 'string')
+			throw new Error('Must be a string!');
+
+		var message = "";
+
+		for(var i = 0; i < s.length; i++){
+
+			var r = s.charAt(i);
+			atRotor = rotorSettings.length - 1;
+
+			for(var j = rotorSettings.length - 1; j >= 0; j--){
+				r = deRotorize(r);
+			}
+			// r = dePlugify(r);
+			message = message + r;
+		}
+
+		return message;
+
+	} catch(e){
+		console.log('Error: ' + e.message);
+	}
+}//end of deEnigmate
+
+function deRotorize(c){
+	if(typeof c !== 'string')
+		throw new Error('Must enter a single character string into deRotorize!');
+	if(c.length > 1)
+		throw new Error('Must only enter one character!');
+	if(sequence === 'capAlpha'){
+
+		var k = c.charCodeAt(0);
+		var n = rotorSettings.charCodeAt(atRotor);
 
 
-console.log(enigmate("BOOM"));
+		atRotor--;
+
+		console.log(k,n);
+
+		// k = k - 65;
+		// n = n - 65;
+
+		// var out = String.fromCharCode((k + n) % 26 + 65);
+
+		// return out;
+
+		k = k - 65;
+		n = n - 65;
+
+		console.log(k + " " + n + '\n');
+
+		var out = String.fromCharCode((k - n) % 26 + 65);
+		
+		return out;
+		//end of capAlpha
+	}
+}//end of deRotorize
+
+function dePlugify(c){
+	if(typeof c !== 'string')
+		throw new Error('Must enter a single character string into rotorize!');
+	if(c.length > 1)
+		throw new Error('Must only enter one character!');
+	if(sequence === 'capAlpha'){
+
+		var k = c.charCodeAt(0);
+
+		for(var i = 0; i < plugSettings.length; i++){
+
+			var t = plugSettings[i].charAt(1);
+
+			if(!(t.charCodeAt(0) >= 65 && t.charCodeAt(0) <= 90))
+				throw new Error('Can only use capital letters in PLUG SETTINGS when SEQUENCE is set to "capAlpha"! \nEither change your SEQUENCE to the appropriate decyphering method, or change your plug settings to be only cap letters');
+			if(t === plugSettings[i].charAt(0))
+				throw new Error('Must use a different letter to connect to in PLUG SETTINGS');
+			if(c === plugSettings[i].charAt(0)){
+				return t;
+			}
+		}
+	}
+	return c;
+}//end of dePlugify
+
+console.log(enigmate("APPLE"));
+console.log(deEnigmate(enigmate("APPLE")));
