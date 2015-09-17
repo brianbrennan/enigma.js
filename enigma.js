f90bccedfea7d3d6e74267af916cf11afaf5a75f
Brian Brennan <brianb@cardsandpockets.com>
2015-09-17 12:29:38 -0400

    seemingly working

---
+++ enigma.js
@@ -0,0 +1,105 @@
+//Settings
+var rotorSettings = "WAADERISTGUT";
+var atRotor = 0;
+
+//Functions
+
+function enigmate(s){
+	var message = "";
+	var saveSettings = rotorSettings;
+
+	for(var i = 0; i < s.length; i++){
+		step();
+
+		var addChar = s[i];
+
+		for(var j = 0; j < rotorSettings.length; j++){
+			addChar = rotorize(addChar);
+		}
+
+		atRotor = 0;
+
+		message = message + addChar;
+	}
+
+	rotorSettings = saveSettings;
+
+	return message;
+}
+
+function rotorize(c){
+
+	var givenCharCode = c.charCodeAt(0) - 65;
+	var rotorCharCode = rotorSettings.charCodeAt(atRotor) - 65;
+	atRotor++;
+
+	if(givenCharCode == -33)
+		return ' ';
+
+	var num = (givenCharCode + rotorCharCode + (26 * rotorSettings.length)) % 26 + 65;
+
+	return String.fromCharCode(num);
+}
+
+function step(){
+	var newSettings = "";
+	for(var i = 0; i < rotorSettings.length; i++){
+		newSettings = newSettings + String.fromCharCode(rotorSettings.charCodeAt(i) + 1);
+	}
+
+	rotorSettings = newSettings;
+}
+
+function decrypt(s){
+	var message = "";
+	var saveSettings = rotorSettings;
+	atRotor = rotorSettings.length - 1;
+
+	for(var i = 0; i < s.length; i++){//get to point that rotor settings should be
+		step();
+	}
+
+	for(var i = s.length - 1; i >= 0; i--){
+		var addChar = s[i];
+
+		for(var j = rotorSettings.length - 1; j >= 0; j--){
+			addChar = deRotorize(addChar);
+		}
+		atRotor = rotorSettings.length - 1;
+		deStep();
+
+		message = addChar + message;
+	}
+
+	rotorSettings = saveSettings;
+
+	return message;
+}
+
+function deRotorize(c){
+	var givenCharCode = c.charCodeAt(0) - 65;
+	var rotorCharCode = rotorSettings.charCodeAt(atRotor) - 65;
+
+	if(givenCharCode == -33)
+		return ' ';
+	atRotor--;
+
+	var num = (givenCharCode - rotorCharCode + (26 * rotorSettings.length)) % 26 + 65;
+
+	return String.fromCharCode(num);
+}
+
+function deStep(){
+	var newSettings = "";
+	for(var i = 0; i < rotorSettings.length; i++){
+		newSettings = newSettings + String.fromCharCode(rotorSettings.charCodeAt(i) - 1);
+	}
+
+	rotorSettings = newSettings;
+}
+
+console.log(enigmate('HELLO THERE THIS IS'));
+
+console.log(decrypt('ZIBNC FFONM ZZMI WS'))
+
+
